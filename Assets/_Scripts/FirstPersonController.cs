c4c851f84c9a742f52a87b4497c8b00d099a75a9
unknown <glogovacu@gmail.com>
2024-05-31 17:08:59 +0200

    First Person Working

--- Assets/_Scripts/FirstPersonController.cs
+++ Assets/_Scripts/FirstPersonController.cs
@@ -7,6 +7,8 @@ public class FirstPersonController : MonoBehaviour {
 
     [SerializeField] private float _speed = 5f;
 
+    [SerializeField] private Transform _cameraTransform;
+
     private Rigidbody _rb;
     private Vector2 _moveInput;
 
@@ -15,7 +17,7 @@ public class FirstPersonController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        Vector3 move = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+        Vector3 move = _cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
         move.y = 0;
         _rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
     }
