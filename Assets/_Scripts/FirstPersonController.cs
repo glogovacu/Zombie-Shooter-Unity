096877f2cdfc2e6045efd3bfe2d640ab1e211bf8
unknown <glogovacu@gmail.com>
2024-06-01 23:10:57 +0200

    Gun Follows Player

--- Assets/_Scripts/FirstPersonController.cs
+++ Assets/_Scripts/FirstPersonController.cs
@@ -17,11 +17,24 @@ public class FirstPersonController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        RotatePlayer();
+        MovePlayer();
+    }
+
+    private void RotatePlayer() {
+        // Rotate the player based on the camera's X and Y rotation
+        Vector3 cameraRotation = _cameraTransform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, 0);
+
+    }
+
+    private void MovePlayer() {
+        // Move the player based on input and camera's forward and right directions
         Vector3 move = _cameraTransform.forward * _moveInput.y + _cameraTransform.right * _moveInput.x;
-        move.y = 0;
+        move.y = 0; // Ensure the player does not move vertically
         _rb.AddForce(move.normalized * _speed, ForceMode.VelocityChange);
     }
-    
+
     public void OnMove(InputAction.CallbackContext context) {
         _moveInput = context.ReadValue<Vector2>();
     }
